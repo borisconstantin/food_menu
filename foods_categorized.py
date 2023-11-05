39a98a0dd983177b25dc6c91c975a9e67ada4f06
borisconstantin <54483884+borisconstantin@users.noreply.github.com>
2023-11-05 20:58:38 +0100

    Add files via upload

---
+++ foods_categorized.py
@@ -0,0 +1,43 @@
+breakfast = {"beignet de farine" : ["lait", "bouillie de mais", "bouillie de mil"],
+         "beignet mais": ["bouillie de mais", "lait"],
+         "pain" : ["lait", "bouillie de mais", "bouillie de mil"],
+         "gateau" : ["lait", "bouillie"],
+         "crêpes sucrées" : ["lait"],
+         "crêpes salées" : ["lait"]
+         }
+
+
+lunch = {"riz blanc" :  ["sauce arachide poisson", "sauce arachide poulet", "sauce tomate poisson", "sauce tomate porc", "sauce tomate poulet",
+                         "sauce oignons poulet", "sauce graine porc", "sauce graine poisson", "sauce graine poulet" ,"sauce pistache poisson",
+                         "sauce pistache poulet", "sauce blanche poisson frit", "sauce béchamel poulet"],
+         "patate bouillie" : ["sauce arachide poisson", "sauce arachide poulet", "ndolè crevettes poulet", "ndolè crevettes viande de boeuf",
+                               "feuille de manioc poisson", "feuille de manioc poulet", "feuille de patate poisson", "feuille de patate poulet"],
+         "igname" : ["sauce arachide poisson", "sauce arachide poulet", "ndolè crevettes poulet", "ndolè crevettes viande de boeuf", "feuille de manioc poisson",
+                      "feuille de manioc poulet", "feuille de patate"],
+         "igname pilé" : ["sauce arachide poisson", "sauce arachide poulet"],
+         "plantain mûr" : ["sauce arachide poisson", "sauce arachide poulet", "sauce tomate", "ndolè crevettes poulet", "ndolè crevettes viande de boeuf", "feuille de manioc poisson", 
+                           "feuille de manioc poulet", "feuille de patate"],
+         "rizoto" : ["poulet"],
+         "plantain mûr pilé" : ["sauce arachide poisson", "sauce arachide poulet"],
+         "frites de plantain" : ["ndolè crevettes poulet", "ndolè crevettes viande de boeuf", "sauce verte piment poulet", "sauce verte piment porc", 
+                                 "sauce verte piment mouton", "sauce verte piment agneau", "sauce verte piment poisson braisé"],
+         "frites de pommes" :  ["ndolè crevettes poulet", "ndolè crevettes viande de boeuf", "sauce verte piment poulet", "sauce verte piment porc", 
+                                 "sauce verte piment mouton", "sauce verte piment agneau", "sauce verte piment poisson braisé"],
+         "bâton de manioc" :  ["ndolè crevettes poulet", "ndolè crevettes viande de boeuf", "sauce verte piment poulet", "sauce verte piment porc", 
+                                 "sauce verte piment mouton", "sauce verte piment agneau", "sauce verte piment poisson braisé"],
+         "atiéké" : ["fritures de tomates piment poisson"],
+         "amiwo" : ["jus de tomates piment poulet", "jus de tomates poulet", "sauce oignons poulet"],
+         "pâte de mais" : ["ndolè crevettes poulet", "ndolè crevettes viande de boeuf", "feuille de manioc poisson", "feuille de manioc poulet", "feuille de patate", "sauce pistache"],
+         "waterfufu" : ["eru peau de boeuf"],
+         "piron (gari)" : ["eru peau de boeuf", "sauce verte piment poulet", "sauce verte piment porc", "sauce verte piment mouton", "sauce verte piment agneau"],
+         "piron (gari) rouge" : ["sauce verte piment poulet", "sauce verte piment porc", "sauce verte piment mouton", "sauce verte piment agneau"],
+         "fufu": ["sauce pistache poisson", "sauce pistache poulet"]
+         }
+
+
+dinner = {"spaghetti" : ["sauce tomate poisson", "sauce tomate poulet"],
+          "pain" : ["bouillon de porc", "bouillon de poulet", "spaghetti sauté poisson"],
+          "plantain mûr bouilli" : ["bouillon de porc", "bouillon de poulet", "spaghetti sauté"],
+          "bâton de manioc" : ["bouillon de porc", "bouillon de poulet", "spaghetti sauté"],
+          "riz blanc" : ["sauce tomate (huile rouge) "],
+          "rizoto" : ["poulet"]}
---
+++ menu_composer.py
@@ -0,0 +1,36 @@
+from foods_categorized import *
+import random
+import pandas as pd
+import streamlit as st
+import time
+
+week_days = ['lundi', 'mardi', 'mercredi', 'jeudi', 'vendredi', 'samedi', 'dimanche']
+
+st.set_page_config(page_title="week foods", layout="wide")
+
+st.title('Weekly menu generator')
+
+generate = st.button('Generate this week food')
+
+if generate : 
+    with st.spinner('Generating menu...') : 
+        time.sleep(5)
+        week_breaksfasts = {}
+        week_lunchs = {}
+        week_dinners = {}
+
+        menu = {}
+
+        for day in week_days : 
+
+            accompanying_bf = random.choice(list(breakfast.keys()))
+            accompanying_lc = random.choice(list(lunch.keys()))
+            accompanying_dn = random.choice(list(dinner.keys()))
+
+            menu[day] = {'ptit dej' : f'{accompanying_bf} + {random.choice(breakfast[accompanying_bf])}',
+                        'Dejeuner' : f'{accompanying_lc} + {random.choice(lunch[accompanying_lc])}',
+                        'Dîner' : f'{accompanying_dn} + {random.choice(dinner[accompanying_dn])}'}
+
+        df = pd.DataFrame(menu).T
+
+        st.dataframe(df)
